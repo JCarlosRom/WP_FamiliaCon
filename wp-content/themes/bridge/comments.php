c7b62f8c09902bc65efd36141a2b20eed6d4fd2b
JCarlosRom <jcarlosrom97@hotmail.com>
2020-04-06 12:33:01 -0500

    06/04/2020

--- wp-content/themes/bridge/comments.php
+++ wp-content/themes/bridge/comments.php
@@ -1,8 +1,8 @@
 <div class="comment_holder clearfix" id="comments">
-<div class="comment_number"><div class="comment_number_inner"><h5><?php comments_number( esc_html__('No Comments','bridge'), '1 '.esc_html__('Comment','bridge'), '% '.esc_html__('Comments','bridge')); ?></h5></div></div>
+<div class="comment_number"><div class="comment_number_inner"><h5><?php comments_number( esc_html__('Sin comentarios','bridge'), '1 '.esc_html__('Comentario','bridge'), '% '.esc_html__('Comentarios','bridge')); ?></h5></div></div>
 <div class="comments">
 <?php if ( post_password_required() ) : ?>
-				<p class="nopassword"><?php esc_html_e( 'This post is password protected. Enter the password to view any comments.', 'bridge' ); ?></p>
+				<p class="nopassword"><?php esc_html_e( 'Este post tiene una contraseña protegida, Ingresa la contraseña para ver los comentarios.', 'bridge' ); ?></p>
 			</div></div>
 <?php
 		
@@ -26,7 +26,7 @@
 
 	 
 		<!-- If comments are closed. -->
-		<p><?php esc_html_e('Sorry, the comment form is closed at this time.', 'bridge'); ?></p>
+		<p><?php esc_html_e('El formulario de comentarios no está disponible.', 'bridge'); ?></p>
 
 	<?php endif; ?>
 <?php endif; ?>
@@ -39,20 +39,22 @@ $bridge_qode_consent  = empty( $bridge_qode_commenter['comment_author_email'] ) 
 $bridge_qode_args = array(
 	'id_form' => 'commentform',
 	'id_submit' => 'submit_comment',
-	'title_reply'=>'<h5>'. esc_html__( 'Post A Comment','bridge' ) .'</h5>',
+	'title_reply'=>'<h5>'. esc_html__( 'Realiza un comentario','bridge' ) .'</h5>',
 	'title_reply_to' => esc_html__( 'Post A Reply to %s','bridge' ),
 	'cancel_reply_link' => esc_html__( 'Cancel Reply','bridge' ),
-	'label_submit' => esc_html__( 'Submit','bridge' ),
-	'comment_field'        => apply_filters( 'qode_comment_form_textarea_field', '<textarea id="comment" placeholder="' . esc_html__( 'Write your comment here...', 'bridge' ) . '" name="comment" cols="45" rows="8" aria-required="true"></textarea>' ),
+	'label_submit' => esc_html__( 'Enviar','bridge' ),
+	'comment_field'        => apply_filters( 'qode_comment_form_textarea_field', '<textarea id="comment" placeholder="' . esc_html__( 'Realiza un comentario aquí', 'bridge' ) . '" name="comment" cols="45" rows="8" aria-required="true"></textarea>' ),
 	'comment_notes_before' => '',
 	'comment_notes_after' => '',
 	'fields' => apply_filters( 'comment_form_default_fields', array(
-		'author' => '<div class="three_columns clearfix"><div class="column1"><div class="column_inner"><input id="author" name="author" placeholder="'. esc_html__( 'Your full name','bridge' ) .'" type="text" value="' . esc_attr( $bridge_qode_commenter['comment_author'] ) . '"' . $bridge_qode_aria_req . ' /></div></div>',
-		'email' => '<div class="column2"><div class="column_inner"><input id="email" name="email" placeholder="'. esc_html__( 'E-mail address','bridge' ) .'" type="text" value="' . esc_attr(  $bridge_qode_commenter['comment_author_email'] ) . '"' . $bridge_qode_aria_req . ' /></div></div>',
+		'author' => '<div class="three_columns clearfix"><div class="column1"><div class="column_inner"><input id="author" name="author" placeholder="'. esc_html__( 'Indica tu nombre','bridge' ) .'" type="text" value="' . esc_attr( $bridge_qode_commenter['comment_author'] ) . '"' . $bridge_qode_aria_req . ' /></div></div>',
+		'email' => '<div class="column2"><div class="column_inner"><input id="email" name="email" placeholder="'. esc_html__( 'Indica tu correo electrónico','bridge' ) .'" type="text" value="' . esc_attr(  $bridge_qode_commenter['comment_author_email'] ) . '"' . $bridge_qode_aria_req . ' /></div></div>',
 		'url' => '<div class="column3"><div class="column_inner"><input id="url" name="url" type="text" placeholder="'. esc_html__( 'Website','bridge' ) .'" value="' . esc_attr( $bridge_qode_commenter['comment_author_url'] ) . '" /></div></div></div>',
 		'cookies' => '<p class="comment-form-cookies-consent"><input id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"' . $bridge_qode_consent . ' />' .
 					'<label for="wp-comment-cookies-consent">' . esc_html__( 'Save my name, email, and website in this browser for the next time I comment.', 'bridge' ) . '</label></p>',
 		  ) ) );
+	
+		
 
 $bridge_qode_args = apply_filters( 'bridge_qode_filter_comment_form_final_fields', $bridge_qode_args );
  ?>
--- wp-content/themes/bridge/includes/comment/comment.php
+++ wp-content/themes/bridge/includes/comment/comment.php
@@ -21,7 +21,7 @@ function bridge_qode_comment($comment, $args, $depth) {
         <?php } ?>
 		<div class="text">
 			<h5 class="name"><?php if ( $is_pingback_comment ) { esc_html_e( 'Pingback:', 'bridge' ); } ?><?php echo get_comment_author_link(); ?></h5>
-			<span class="comment_date"><?php esc_html_e('Posted at', 'bridge'); ?> <?php comment_date('H:i'); ?>h, <?php comment_date('d F'); ?></span>
+			<span class="comment_date"><?php esc_html_e('Publicado a las: ', 'bridge'); ?> <?php comment_date('H:i'); ?>h, <?php comment_date('d F'); ?></span>
 			<?php comment_reply_link( array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']) ) ); ?>
 			<div class="text_holder" id="comment-<?php echo comment_ID(); ?>">
 				<?php comment_text(); ?>
